b3f3e7adb7143668b19ad5cd26aabe8d2547ec07
BALAJI <dbalajivaraprasad@gmail.com>
2025-01-16 23:22:54 +0530

    ```new aliases```

--- aliases.cmd
+++ aliases.cmd
@@ -1,6 +1,9 @@
+REM HACK:
 jn=jupyter notebook
-jl=jupyterlab
-gh=cd Documents\Projects
-ls=dir
+jl=jupyter lab
+gh=cd C:\Users\dbala\Documents\Projects
+ls=dir /a:-s REM shows directories, files and hidden files and directories
 clear=cls
-cdh=cd %homedrive%%homepath%
+cdh=cd %homedrive%%homepath% REM return to home command prompt home directory
+nvimd = cd C:\Users\dbala\AppData\Local\nvim REM Holds neovim configuration files
+python3 = python
