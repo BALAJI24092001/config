c7fb04bb4a79e0c57cedc1c60a80a6d4f55ea551
BALAJI <dbalajivaraprasad@gmail.com>
2025-03-28 01:24:34 +0530

    terminal configs

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+# Creating permanant alias in ps
+# $PROFILE gives the profile file location
+# edit the profile by going to the location and editing the file there, or by using the code below
+# nvim $PROFILE
+new-alias -Name gh -Value github_dir
+new-alias -Name study -Value study_dir
+new-alias -Name nvimd -Value nvim_dir
+new-alias -Name onescroll -Value C:\Users\dbala\.config\"Horizontal Scrolling Onenote.ahk"
+new-alias -Name cdh -Value cd_home
+function github_dir {
+    cd C:\Users\dbala\Documents\Projects;
+}
+function study_dir {
+    cd C:\Users\dbala\Documents\Study
+}
+function nvim_dir {
+    cd C:\Users\dbala\AppData\Local\nvim
+}
+function cd_home {
+    cd $HOME
+}
--- aliases.cmd
+++ aliases.cmd
@@ -6,4 +6,7 @@ ls=dir /a:-s REM shows directories, files and hidden files and directories
 clear=cls
 cdh=cd %homedrive%%homepath% REM return to home command prompt home directory
 nvimd = cd C:\Users\dbala\AppData\Local\nvim REM Holds neovim configuration files
-python3 = python
+study = cd  C:\Users\dbala\Documents\Study
+ls=dir /a:-s
+onescroll = C:\Users\dbala\.config\"Horizontal Scrolling Onenote.ahk"
+
